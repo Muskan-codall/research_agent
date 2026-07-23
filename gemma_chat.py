88fb3150a6b9c1ca235619612eb9c40ee4e848d2
muskan-codall <mukkuusahu27@gmail.com>
2026-07-24 00:55:34 +0530

    perform commit

---
+++ gemma_chat.py
@@ -0,0 +1,20 @@
+import ollama
+
+def ask_gemma(prompt):
+
+    response = ollama.chat(
+        model="gemma4:12b",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response["message"]["content"]
+
+
+question = input("You : ")
+
+print(ask_gemma(question))
---
+++ main.py
@@ -0,0 +1,4 @@
+from calculator import calculate
+from gemma_chat import ask_gemma
+
+
